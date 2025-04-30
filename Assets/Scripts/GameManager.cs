4c40cdf2c2bb768e4f0fe03c38938661cda4055b
Vinicius Mattei <viniciuspmattei@gmail.com>
2025-04-30 13:28:04 -0300

    Spawning crosses when clicking

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,16 @@
+using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
+
+    public event EventHandler<OnClickedOnGridPositionEventArgs> OnClickedOnGridPosition;
+    public class OnClickedOnGridPositionEventArgs : EventArgs
+    {
+        public int x;
+        public int y;
+    }
 
     private void Awake()
     {
@@ -10,11 +18,16 @@ public class GameManager : MonoBehaviour
         {
             Debug.LogError("More than one GameManager Instance!");
         }
-        Instance = this; 
+        Instance = this;
     }
 
     public void ClickedOnGridPosition(int x, int y)
     {
         Debug.Log("Clicked on the grid: " + x + ", " + y);
+        OnClickedOnGridPosition.Invoke(this, new OnClickedOnGridPositionEventArgs
+        {
+            x = x,
+            y = y
+        });
     }
 }
---
+++ Assets/Scripts/GameVisualManager.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+public class GameVisualManager : MonoBehaviour
+{
+    private const float GRID_SIZE = 3.1f;
+
+    [SerializeField] private Transform crossPrefab;
+    [SerializeField] private Transform circlePrefab;
+
+    void Start()
+    {
+        GameManager.Instance.OnClickedOnGridPosition += GameManager_OnClickedOnGridPosition;
+    }
+
+    private void GameManager_OnClickedOnGridPosition(object sender, GameManager.OnClickedOnGridPositionEventArgs e)
+    {
+        Instantiate(crossPrefab, GetGridWorldPosition(e.x, e.y), Quaternion.identity);
+    }
+
+    private Vector2 GetGridWorldPosition(int x, int y)
+    {
+        return new Vector2(-GRID_SIZE + x * GRID_SIZE, -GRID_SIZE + y * GRID_SIZE);
+    }
+}
