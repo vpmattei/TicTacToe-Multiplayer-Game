6e78bd76edbe9170c200e233e5bb166530de3b93
Vinicius Mattei <viniciuspmattei@gmail.com>
2025-05-07 14:56:16 -0300

    Implemented win and lose sfx and music background

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
     [SerializeField] private Transform placeSfxPrefab;
+    [SerializeField] private Transform winSfxPrefab;
+    [SerializeField] private Transform loseSfxPrefab;
 
     void Start()
     {
         GameManager.Instance.OnPlacedObject += GameManager_OnPlacedObject;
+        GameManager.Instance.OnGameWin += GameManager_OnGameWin;
+    }
+
+    private void GameManager_OnGameWin(object sender, GameManager.OnGameWinEventArgs e)
+    {
+        if (GameManager.Instance.GetLocalPlayerType() == e.winPlayerType)
+        {
+            Transform sfxTransform = Instantiate(winSfxPrefab);
+            Destroy(sfxTransform.gameObject, 3f);
+        }
+        else
+        {
+            Transform sfxTransform = Instantiate(loseSfxPrefab);
+            Destroy(sfxTransform.gameObject, 8f);
+        }
     }
 
     private void GameManager_OnPlacedObject(object sender, EventArgs e)
     {
         Transform sfxTransform = Instantiate(placeSfxPrefab);
-        Destroy(sfxTransform.gameObject, 5f);
+        Destroy(sfxTransform.gameObject, .6f);
     }
 }
